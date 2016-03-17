6878a1d16631ebd3566d61db4d37849885331af5
Antti Kurronen <antti.kurronen@gmail.com>
2016-03-17 09:21:29 +0200

    adding CentroidFromNPoints to index

---
+++ MC-Cone/src/information/CentroidFromNpoints.java
@@ -0,0 +1,53 @@
+package information;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+/**
+ * The Class CentroidFromNpoints. Contains a Centeroid point and from how many points it is composed.
+ */
+public class CentroidFromNpoints extends Point{
+
+	
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 3096112387384468511L;
+	
+	/** The Constant LOGGER. */
+	private final static Logger LOGGER = Logger.getLogger("MCCLogger");
+
+	/** The number of coordinates. */
+	private int numberOfCoordinates=0;
+	
+	public CentroidFromNpoints(Point point, int amount){
+		super(point);
+		try {
+			this.setNumberOfCoordinates(amount);
+		} catch (Exception e) {
+			LOGGER.severe("Error in creating CenteoidFromNPoints");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns the number of coordinates.
+	 *
+	 * @return the number of coordinates
+	 * @throws Exception the exception
+	 */
+	public int getNumberOfCoordinates() throws Exception{
+		return numberOfCoordinates;
+	}
+
+	/**
+	 * Sets the number of coordinates.
+	 *
+	 * @param numberOfCoordinates the new number of coordinates
+	 * @throws Exception the exception
+	 */
+	public void setNumberOfCoordinates(int numberOfCoordinates) throws Exception {
+		this.numberOfCoordinates = numberOfCoordinates;
+	}
+	
+	
+}
